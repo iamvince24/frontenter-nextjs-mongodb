feefd9fe9b5be7bbcf4ad9a052ec560d1dec7990
iamvince24 <yohongguo24@gmail.com>
2025-03-01 20:37:21 +0800

    comment unneeded article form

--- src/app/profile/article/create/page.tsx
+++ src/app/profile/article/create/page.tsx
@@ -1,18 +1,12 @@
-import * as React from "react";
-import Link from "next/link";
-import { NavButton } from "@/components/ui/NavButton";
-import ArticleForm from "@/components/article/ArticleForm";
-import { getCurrentUser } from "@/actions/getCurrentUser";
+import * as React from 'react'
+import ArticleForm from '@/features/article/pages/ArticleFormPage'
+import { getCurrentUser } from '@/actions/getCurrentUser'
 
 const Create = async () => {
-  const currentUser = await getCurrentUser();
-  const authorId = currentUser?.id;
+  const currentUser = await getCurrentUser()
+  const authorId = currentUser?.id
 
-  return (
-    <div>
-      <ArticleForm authorId={authorId} />
-    </div>
-  );
-};
+  return <ArticleForm authorId={authorId} />
+}
 
-export default Create;
+export default Create
