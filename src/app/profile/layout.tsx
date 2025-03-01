f86a5db9a116d8eaf1e942e7ce7521deba07f8a9
iamvince24 <yohongguo24@gmail.com>
2025-03-01 11:25:03 +0800

    add improve profile layout

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,29 +1,34 @@
-import * as React from "react";
-import Link from "next/link";
-import { NavButton } from "@/components/ui/NavButton";
+'use client'
 
-export default async function ProfileTab({
+import * as React from 'react'
+import Link from 'next/link'
+import { NavButton } from '@/components/ui/NavButton'
+import { usePathname } from 'next/navigation'
+
+export default function ProfileTab({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
+  const pathname = usePathname()
+
+  const navItems = [
+    { path: '/profile', label: '會員資料' },
+    { path: '/profile/article/collection', label: '文章蒐藏' },
+    { path: '/profile/article/self', label: '自己的文章' },
+    { path: '/profile/article/create', label: '新增文章' },
+  ]
+
   return (
     <div>
       <div className="flex justify-center my-5">
-        <Link href="/profile">
-          <NavButton>會員資料</NavButton>
-        </Link>
-        <Link href="/profile/article/collection">
-          <NavButton>文章蒐藏</NavButton>
-        </Link>
-        <Link href="/profile/article/self">
-          <NavButton>自己的文章</NavButton>
-        </Link>
-        <Link href="/profile/article/create">
-          <NavButton>新增文章</NavButton>
-        </Link>
+        {navItems.map(item => (
+          <Link href={item.path} key={item.path}>
+            <NavButton active={pathname === item.path}>{item.label}</NavButton>
+          </Link>
+        ))}
       </div>
       {children}
     </div>
-  );
+  )
 }
