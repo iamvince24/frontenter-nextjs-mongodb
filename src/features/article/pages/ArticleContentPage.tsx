1467bf540b32cd7883e0ec9aa6612c28baf2c022
iamvince24 <yohongguo24@gmail.com>
2025-03-24 00:52:34 +0800

    improve ArticleContentPage rwd

--- src/features/article/pages/ArticleContentPage.tsx
+++ src/features/article/pages/ArticleContentPage.tsx
@@ -17,18 +17,22 @@ export default function ArticleContentPage({ article }: ArticleContentPageProps)
   const { title, imageUrl, author, content, updatedAt } = article
 
   return (
-    <div className="flex flex-col gap-4 items-center px-60">
-      <div className="flex flex-col gap-4 items-start mt-16">
-        <h1 className="text-[40px] font-bold">{title}</h1>
+    <div className="flex flex-col gap-4 items-center px-10 md:px-40 lg:px-60 mb-10">
+      <div className="flex flex-col gap-4 items-start mt-16 ">
+        <h1 className="text-[28px] sm:text-[40px] font-bold mb-1 sm:mb-0">{title}</h1>
 
-        <div className="flex gap-8">
+        <div className="flex flex-col sm:flex-row gap-2 sm:gap-8">
           <div className="text-[16px] mt-[-10px]">作者：{author?.username}</div>
           <div className="text-[16px] mt-[-10px]">更新日期：{dayjs(updatedAt).format('YYYY-MM-DD')}</div>
         </div>
 
-        {imageUrl && <Image src={imageUrl} alt={title} width={1000} height={400} />}
+        {imageUrl && (
+          <div className="w-full md:w-[700px] lg:w-[900px] xl:w-[1080px] aspect-[16/9] relative overflow-hidden">
+            <Image src={imageUrl} alt={title} width={1200} height={675} />
+          </div>
+        )}
       </div>
-      <div className="w-4/5 flex gap-4 justify-start mt-16">
+      <div className="w-full flex gap-4 text-justify justify-start mt-16">
         <ArticleContent content={content} />
       </div>
     </div>
